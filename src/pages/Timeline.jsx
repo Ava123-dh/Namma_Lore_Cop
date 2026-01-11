dfc31ee6df5098b438950546d3b7e62369a5ce6b
Ava123-dh <avani.venkatesh.rao@gmail.com>
2026-01-11 11:32:14 +0530

    some filter

--- src/pages/Timeline.jsx
+++ src/pages/Timeline.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+\import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { Heart, Calendar, Users, MapPin, ChevronRight } from 'lucide-react'
 import { useFavorites } from '../context/FavoritesContext'
