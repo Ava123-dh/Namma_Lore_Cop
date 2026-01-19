238e342130ed3f8867797c5da8172c3420defc51
Ava123-dh <avani.venkatesh.rao@gmail.com>
2026-01-19 18:46:24 +0530

    life is WORTH A LIVINGGGG

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Namma_Lore_Cop/',
+  base: '/',
   plugins: [react()],
   server: {
     port: 3000,
